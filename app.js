60345a32ae9dd0281b4ba30353269befe9a6615b
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-01-02 13:55:29 +0530

    refactor: update UI based on local storage

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const totalExpense = document.querySelector('#total-expense');
 const formAddExpense = document.querySelector('#form-add-expense');
 const expenseContainer = document.querySelector('.expense-container');
-const totalExpense = document.querySelector('.total-expense');
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -9,83 +9,72 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+
 class Expense {
-  constructor(title, amt) {
+  constructor(title, amount, date) {
     this.id = uuidv4();
     this.title = title;
-    this.amt = amt;
-    this.addedAt = dateFns.format(new Date(), 'MMMM D, YYYY');
+    this.amount = amount;
+    this.date = dateFns.format(new Date(date), 'MMMM D, YYYY');
   }
 }
 
-const expensesInStorage = () => JSON.parse(localStorage.getItem('expenses'));
+const getExpenses = () => JSON.parse(localStorage.getItem('expenses'));
 
-const updateStorage = (expenses) => {
-  localStorage.setItem('expenses', JSON.stringify(expenses));
+const updateExpenses = (updatedExpenses) => {
+  localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
+  updateUI();
 };
 
-const addToStorage = (newExpense) => {
-  const expenses = expensesInStorage();
-  expenses.push(newExpense);
-  updateStorage(expenses);
+const addExpense = (newExpense) => {
+  updateExpenses([...getExpenses(), newExpense]);
 };
 
-const removeFromStorage = (expenseID) => {
-  updateStorage(
-    expensesInStorage().filter((expense) => {
-      return expense.id !== expenseID;
+const deleteExpense = (expenseID) => {
+  updateExpenses(getExpenses().filter(({ id }) => id !== expenseID));
+};
+
+const updateUI = () => {
+  expenseContainer.innerHTML = getExpenses()
+    .map(({ id, title, amount, date }) => {
+      return `
+        <li data-id=${id}>
+          <p>${title}</p>
+          <p>₹ ${amount}</p>
+          <small>${date}</small>
+          <i class="fas fa-trash btn-delete"></i>
+        </li>
+        `;
     })
-  );
-};
+    .join('');
 
-const updateTotalExpense = (expenseAmt) => {
-  totalExpense.textContent = Number(totalExpense.textContent) + expenseAmt;
-};
-
-const updateUI = ({ id, title, amt, addedAt }) => {
-  expenseContainer.innerHTML += `
-    <div class="expense-card" data-id="${id}">
-      <div class="expense">
-        <h3 class="expense-title">${title}</h3>
-        <span class="expense-amt">₹ ${amt}</span>
-      </div>
-      <div class="data-secondary">
-        <small class="added-at">${addedAt}</small>
-        <i class="fas fa-trash btn-delete"></i>
-      </div>
-    </div>
-    `;
+  totalExpense.innerHTML = `Total Expense: ${getExpenses().reduce(
+    (sum, { amount }) => {
+      return (sum += Number(amount));
+    },
+    0
+  )}`;
 };
 
 formAddExpense.addEventListener('submit', (e) => {
   e.preventDefault();
   const title = formAddExpense.title.value.trim();
-  const amount = Number(formAddExpense.amount.value);
-  const newExpense = new Expense(title, amount);
-  addToStorage(newExpense);
-  updateUI(newExpense);
-  updateTotalExpense(amount);
+  const amount = formAddExpense.amount.value;
+  const date = formAddExpense.date.value;
+  const expense = new Expense(title, amount, date);
+  addExpense(expense);
   formAddExpense.reset();
 });
-
-const removeExpense = (element) => {
-  removeFromStorage(element.getAttribute('data-id'));
-  element.remove();
-};
 
 expenseContainer.addEventListener('click', (e) => {
   const target = e.target;
   if (target.classList.contains('btn-delete')) {
-    removeExpense(target.parentElement.parentElement);
+    deleteExpense(target.parentElement.getAttribute('data-id'));
   }
 });
 
-if (!expensesInStorage()) {
-  console.log('not found');
-  localStorage.expenses = JSON.stringify([]);
+if (!getExpenses()) {
+  localStorage.setItem('expenses', JSON.stringify([]));
 } else {
-  expensesInStorage().forEach((expense) => {
-    updateTotalExpense(expense.amt);
-    updateUI(expense);
-  });
+  updateUI();
 }
